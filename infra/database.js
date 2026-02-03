a47cfb3cb47166f8453686d10954374c6dcae57d
vinicius-ferrari <vinicius.ferrari@medcof.tech>
2026-02-03 20:18:02 -0300

    Removed logging of PostgreSQL connection credentials in database.js for security reasons. Added a new migration endpoint in index.js to handle database migrations and created a test for the migrations API to ensure it returns a 200 status code.

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,13 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
-  console.log('Credenciais do postgres: ', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });  
+  // console.log('Credenciais do postgres: ', {
+  //   host: process.env.POSTGRES_HOST,
+  //   port: process.env.POSTGRES_PORT,
+  //   user: process.env.POSTGRES_USER,
+  //   database: process.env.POSTGRES_DB,
+  //   password: process.env.POSTGRES_PASSWORD,
+  // });  
 
     try {
     await client.connect();
