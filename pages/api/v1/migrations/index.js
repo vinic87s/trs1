5ccb758b8e89934918e599e9daec393730c5e207
vinicius-ferrari <vinicius.ferrari@medcof.tech>
2026-02-09 15:37:16 -0300

    Refactor migration logic in index.js to correct variable naming and improve error message clarity. Update migration function calls to use consistent naming conventions.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,4 +1,4 @@
-import migrationsRunner from "node-pg-migrate";
+import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database.js";
 
@@ -6,7 +6,7 @@ export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
     return response.status(405).json({
-      error: `Method "${request.method}" not allow`,
+      error: `Method "${request.method}" not allowed`,
     });
   }
 
@@ -17,7 +17,6 @@ export default async function migrations(request, response) {
 
     const defaultMigrationOptions = {
       dbClient: dbClient,
-      databaseUrl: process.env.DATABASE_URL,
       dryRun: true,
       dir: join("infra", "migrations"),
       direction: "up",
@@ -26,12 +25,12 @@ export default async function migrations(request, response) {
     };
 
     if (request.method === "GET") {
-      const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       return response.status(200).json(pendingMigrations);
     }
 
     if (request.method === "POST") {
-      const migratedMigrations = await migrationsRunner({
+      const migratedMigrations = await migrationRunner({
         ...defaultMigrationOptions,
         dryRun: false,
       });
